c2a8e3c4447a5bb3eb9802e61d091331acf32f5a
Erick Peña <erick.pmartinez@hotmail.com>
2024-03-09 19:33:43 -0500

    d

--- src/main/java/ejercicios/Division.java
+++ src/main/java/ejercicios/Division.java
@@ -11,11 +11,23 @@ import java.util.Scanner;
 public class Division {
     public static String evaluar(int dividendo, int divisor) {
         // TODO: Coloca aquí el código del ejercicio 3: Division
-        int cociente = 0;
-        int residuo = 0;
+        if (dividendo%divisor!=0){
+            int cociente = dividendo/divisor;
+            int residuo = dividendo%divisor;
+            String respuesta = "La división no es exacta. \n"
+                    + "Cociente: " + cociente + "\n"
+                    + "Residuo: " + residuo;
+
+            return respuesta;
+
+
+        }
+        int cociente = dividendo/divisor;
+        int residuo = dividendo%divisor;
         String respuesta = "La división es exacta. \n"
                 + "Cociente: " + cociente + "\n"
                 + "Residuo: " + residuo;
+
         return respuesta;
     }
     
