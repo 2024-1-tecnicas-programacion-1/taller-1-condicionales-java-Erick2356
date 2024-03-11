c137e77329e3b92b72bacf25865de6052293ff89
Erick Peña <erick.pmartinez@hotmail.com>
2024-03-11 17:41:19 -0500

    .

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 
 public class SetDeTenis {
     public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        if ((numVictoriasA >= 6 && numVictoriasB <= 4) || (numVictoriasB >= 6 && numVictoriasA <= 4)) {
+        if (numVictoriasA < 0 || numVictoriasA > 7 || numVictoriasB < 0 || numVictoriasB > 7) {
             return "Inválido";
-        } else if (numVictoriasA >= 6 && numVictoriasB >= 6) {
-            if (Math.abs(numVictoriasA - numVictoriasB) == 1) {
-                return "Aún no termina";
-            } else {
-                return (numVictoriasA > numVictoriasB) ? "Ganó A" : "Ganó B";
-            }
+        } else if ((numVictoriasA >= 6 || numVictoriasB >= 6) && Math.abs(numVictoriasA - numVictoriasB) >= 2) {
+            return (numVictoriasA > numVictoriasB) ? "Ganó A" : "Ganó B";
+        } else if ((numVictoriasA >= 6 && numVictoriasB >= 6) && Math.abs(numVictoriasA - numVictoriasB) == 1) {
+            return "Aún no termina";
         } else {
             return "Aún no termina";
         }
     }
+
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
